d890157546caa8dd9330098f6b11ce2422b7826a
olafdolven <48141859+olafdolven@users.noreply.github.com>
2020-12-19 11:50:10 +0100

    extra shits

--- Filer_for_Chat med komplett/events_002.js
+++
@@ -1,9 +0,0 @@
-define(["komplett-pubsub"], function (pubsub) {
-    var events = {
-        loadComparisonWidget: function () {
-        },
-        addedToComparison: function () {
-        }
-    };
-    return pubsub.extend(events);
-});
--- Filer_for_Chat med komplett/events_003.js
+++
@@ -1,9 +0,0 @@
-define(["komplett-pubsub"], function (pubsub) {
-    /* eslint-disable no-unused-vars */
-    var events = {
-        slideClicked: function (number) { },
-        largeGalleryOpened: function (slideIndex) { }
-    };
-    /* eslint-enable no-unused-vars */
-    return pubsub.extend(events);
-});
